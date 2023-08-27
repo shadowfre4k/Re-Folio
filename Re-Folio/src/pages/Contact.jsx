dbf9c490e023126309167688aadf3b5187a03aa7
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-08-27 12:43:49 -0700

    added contact stylings!

--- Re-Folio/src/pages/Contact.jsx
+++ Re-Folio/src/pages/Contact.jsx
@@ -1,6 +1,41 @@
 import { useState } from "react";
 import { validateEmail } from "../util/helpers";
 
+const styles = {
+  container: {
+    width: "100%",
+    height: "100%",
+    padding: "5%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  childContainer: {
+    boxShadow: "0px 10px 20px #cadefc",
+    textShadow: "1px 1px 1px #cca8e9",
+    borderRadius: " 50px",
+    backgroundColor: "#defcf9",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+
+    padding: "2% 10%",
+  },
+
+  form: {
+    margin: "8%",
+    borderRadius: " 50px",
+    textAlign: "center",
+  },
+  bigform: {
+    borderRadius: " 50px",
+    textAlign: "center",
+    width: "500px",
+    height: "300px",
+  },
+};
 export default function Contact() {
   const [userName, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -38,10 +73,11 @@ export default function Contact() {
   };
 
   return (
-    <div>
-      <h1>Please fill out form to Contact</h1>
-      <form onSubmit={handleFormSubmit}>
+    <div style={styles.container}>
+      <form style={styles.childContainer} onSubmit={handleFormSubmit}>
+        <h1>Please fill out form to Contact</h1>
         <input
+          style={styles.form}
           value={userName}
           name="userName"
           onChange={handleInputChange}
@@ -49,6 +85,7 @@ export default function Contact() {
           placeholder="userName"
         />
         <input
+          style={styles.form}
           value={email}
           name="email"
           onChange={handleInputChange}
@@ -56,13 +93,16 @@ export default function Contact() {
           placeholder="email"
         />
         <input
+          style={styles.bigform}
           value={message}
           name="message"
           onChange={handleInputChange}
           type="message"
           placeholder="message"
         />
-        <button type="submit">Submit</button>
+        <button style={styles.form} type="submit">
+          Submit
+        </button>
       </form>
       {errorMessage && (
         <div>
